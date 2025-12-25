f557a7f259902f100c3bcfeadb817d185d6c11cd
srima2003 <167190725+srima2003@users.noreply.github.com>
2025-12-25 22:17:10 +0530

    Update UserService.java

--- src/main/java/com/OpenHearing/Usermanagement/service/UserService.java
+++ src/main/java/com/OpenHearing/Usermanagement/service/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Page;
 public interface UserService {
     UserDTO createUser(UserDTO userDTO);
     UserDTO updateUser(Long id, UserDTO userDTO);
-    Page<UserDTO> getAllUsers(int page, int size);
+    Page<UserDTO> getAllUsers(int page, int size, String keyword);
  // Add this line inside the interface
     void deleteUser(Long id);
 }
